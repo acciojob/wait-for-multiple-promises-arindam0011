d7c59b44fb9194401d223e23dd34b6ca50c6dbc7
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-24 05:16:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
-//your JS code here. If required.
+
+let trow = document.getElementById("output");
+
+let Default = document.createElement("tr");
+Default.innerHTML = `<td colspan="2">Loading...</td>`;
+
+trow.appendChild(Default);
+let totaRandomTime = 0;
+new Promise((resolve, reject) => {
+	let delay=Math.floor(Math.random()*(3000-1000+1)+1000);
+		totaRandomTime += delay;
+	setTimeout(() => {
+		let tr = document.createElement("tr");
+		tr.innerHTML = `<td>Promise 1</td>
+					  <td class="time">${parseInt(delay/1000)}</td>`;
+		trow.insertBefore(tr, Default);
+		resolve();
+	}, delay)
+})
+	.then(() => {
+		let delay=Math.floor(Math.random()*(3000-1000+1)+1000);
+		totaRandomTime += delay;
+		return new Promise((resolve, reject) => {
+			
+			setTimeout(() => {
+				let tr = document.createElement("tr");
+				tr.innerHTML = `<td>Promise 2</td>
+						  <td class="time">${parseInt(delay/1000)}</td>`;
+				trow.insertBefore(tr, Default);
+				resolve();
+			}, delay)
+		})
+	})
+	.then(() => {
+		let delay=Math.floor(Math.random()*(3000-1000+1)+1000);
+			totaRandomTime += delay;
+		return new Promise((resolve, reject) => {
+			
+			setTimeout(() => {
+				let tr = document.createElement("tr");
+				tr.innerHTML = `<td>Promise 3</td>
+						  <td class="time">${parseInt(delay/1000)}</td>`;
+				trow.insertBefore(tr, Default);
+				resolve();
+			}, delay)
+		})
+	})
+	.then(() => {
+
+		return new Promise((resolve, reject) => {
+			let tr = document.createElement("tr");
+			tr.innerHTML = `<td>Total</td>
+						  <td>${totaRandomTime/1000}</td>`;
+			trow.insertBefore(tr, Default);
+			resolve();
+		})
+	}).then(()=>{
+		let allRows = Array.from(document.querySelectorAll("tr"));
+
+		new Promise.all(allRows)
+			.then((rows) => {
+				trow.removeChild(Default);
+			})
+	})
+
