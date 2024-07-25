576b0a56c9469ae34f4b430e7798bf5bb7ee0f3f
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-07-25 23:11:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ let trow = document.getElementById("output");
 // Create and add the loading row
 let Default = document.createElement("tr");
 Default.innerHTML = `<td colspan="2">Loading...</td>`;
-Default.id="loading"
+Default.id="loading";
 trow.appendChild(Default);
 
 let totalTime = 0;
 
 // Function to create a promise that resolves after a random delay
 let createPromise = async (name) => {
-	let delay = Math.floor(Math.random() * (3000 - 1000 + 1) + 1000);
+	let delay = Math.floor(Math.random() * (1000 - 500 ) + 1000);
 	totalTime += delay;
 	let promise = await new Promise((resolve) => {
 		setTimeout(() => {
@@ -24,6 +24,7 @@ let createPromise = async (name) => {
 	return promise;
 }
 
+
 // Create three promises
 let promise1 = createPromise("Promise 1");
 let promise2 = createPromise("Promise 2");
@@ -32,7 +33,6 @@ let promise3 = createPromise("Promise 3");
 // Array of promises
 let promises = [promise1, promise2, promise3];
 
-// Use Promise.all() to wait for all promises to resolve
 Promise.all(promises)
 	.then((results) => {
 		Default.innerHTML = `<td>Total</td>
